03e047e536d7cdceb5ee3846d86c11b1f0a3f825
Dur1lka <polinastex15@yandex.ru>
2025-11-15 23:43:06 +0300

    выполнение домашки

---
+++ lesson6/lesson6_task2.py
@@ -0,0 +1,19 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+driver.get("http://uitestingplayground.com/textinput")
+input=driver.find_element(By.CSS_SELECTOR, '#newButtonName')
+input.send_keys('Skypro')
+click=driver.find_element(By.CSS_SELECTOR, '#updatingButton').click()
+txt=driver.find_element(By.CSS_SELECTOR, '#updatingButton').text
+print(txt)
+
+driver.quit()
